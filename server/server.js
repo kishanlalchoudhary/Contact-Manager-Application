59b3fc70fc24dd2889e8252e5327cadf4e7a6309
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2023-08-31 21:02:12 +0530

    Configured cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,21 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+var whitelist = [
+  "http://localhost:3000/",
+  "https://contact-manager-mern.netlify.app/",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
